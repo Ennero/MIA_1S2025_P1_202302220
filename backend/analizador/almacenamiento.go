bdf9de5790c9aee81321dc9289146c540d9939f5
ennero <2895884051401@ingenieria.usac.edu.gt>
2025-03-19 11:11:36 -0600

    Tremendo copia y pega xd

--- backend/analizador/almacenamiento.go
+++
@@ -1,9 +0,0 @@
-package analizador
-
-// Carnet de estudiante
-const Carnet string = "53" // 202113553
-
-// Declaración de variables globales
-var (
-	MountedPartitions map[string]string = make(map[string]string)
-)
